ea69b0f833904da5d90c085cbfd0c6f90925619a
Mardonis Alves Bezerra <mardonisgp@gmail.com>
2021-02-16 19:14:28 -0300

    deploy-backend

---
+++ src/config/server.js
@@ -0,0 +1,19 @@
+const port = process.env.PORT
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+const AllowCors = require('./cors')
+const queryParser = require('express-query-int')
+
+
+server.use(bodyParser.urlencoded({extended : true}))
+server.use(bodyParser.json())
+server.use(AllowCors)
+server.use(queryParser())
+
+server.listen(port ,function() {
+    console.log("BACKEND is running" , port )
+})
+
+module.exports = server
+
