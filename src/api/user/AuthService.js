80ebb8a5d80e05e1fff3f42e96ccd4ccd9d41030
Mardonis Alves Bezerra <mardonisgp@gmail.com>
2021-02-18 00:25:15 -0300

    remove .env

--- src/api/user/AuthService.js
+++ src/api/user/AuthService.js
@@ -3,9 +3,9 @@ const _ = require('lodash')
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const User = require('./user')
-//const env = process.env.AUTH_SECRET
+const env = process.env.AUTH_SECRET
 
-const env  = require('../../.env')
+
 
 //regex
 const emailRegex = /\S+@\S+\.\S+/
@@ -26,7 +26,7 @@ const login = (req, res, next) => {
         if (err) {
             return sendErrorsFromDB(res, err)
         } else if (user && bcrypt.compareSync(password, user.password)) {
-            const token = jwt.sign(user, env.authSecret, {
+            const token = jwt.sign(user, env, {
                 expiresIn: "1 day"
             })
             const { name, email } = user
@@ -40,7 +40,7 @@ const login = (req, res, next) => {
 // validate token
 const validateToken = (req, res, next) => {
     const token = req.body.token || ''
-    jwt.verify(token, env.authSecret, function (err, decoded) {
+    jwt.verify(token, env, function (err, decoded) {
         return res.status(200).send({ valid: !err })
     })
 }
